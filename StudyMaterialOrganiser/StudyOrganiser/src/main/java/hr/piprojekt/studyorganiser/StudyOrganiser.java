d7c7fa981ad1acf35a5e3808a2f8f022667e952c
Maja Pacak <maja.pacak1@outlook.com>
2024-12-06 22:54:21 +0100

    adding module

---
+++ StudyMaterialOrganiser/StudyOrganiser/src/main/java/hr/piprojekt/studyorganiser/StudyOrganiser.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hr.piprojekt.studyorganiser;
+
+/**
+ *
+ * @author majap
+ */
+public class StudyOrganiser {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
